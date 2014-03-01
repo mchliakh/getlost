1cb94d8c9c213c83214ab3eec53d0c1751bbe524
Mikhail Chliakhovski <mchliakh@gmail.com>
2014-03-01 16:28:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parks::Application.config.secret_key_base = 'd69bbbd22f122a9f851ceb1e9cba5666536e733002f9ce692dead2cfc0cf083eaaff5fea046f0eedf37373a8c9aa665f0865c802bf07ea31ccf5ad007411b7fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Parks::Application.config.session_store :cookie_store, key: '_parks_session'
